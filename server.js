a81e6bb12a28722948be79e4865276b2f9f8731d
Your Name <xen@xen.ru>
2021-06-06 22:07:13 +0300

    After lesson 8

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(
   createProxyMiddleware({ target: STRAPI_URL, changeOrigin: true })
 )
 
+
 app.get('/', async (req, res) => {
   const categoriesOnMainRaw = await fetch(`${STRAPI_URL}/page-home-settings?name=category_on_home`)
   const categoriesOnMain = await categoriesOnMainRaw.json()
@@ -25,6 +26,32 @@ app.get('/', async (req, res) => {
 
   return res.render('pages/home', { productsOnMain })
 })
+
+
+app.get('/catalog', async (req, res) => {
+  const categoriesRaw = await fetch(`${STRAPI_URL}/categories`)
+  const categories = await categoriesRaw.json()
+
+  return res.render('pages/catalog', { categories })
+})
+
+
+app.get('/catalog/:categoryId', async (req, res) => {
+  const productsInCategoryRaw = await fetch(`${STRAPI_URL}/products?categories=${req.params.categoryId}`)
+  const productsInCategory = await productsInCategoryRaw.json()
+
+  return res.render('pages/catalog-products-in-category', { productsInCategory })
+})
+
+
+app.get('/products/:id', async (req, res) => {
+  const productDetailsRaw = await fetch(`${STRAPI_URL}/products/${req.params.id}`)
+  const productDetails = await productDetailsRaw.json()
+
+  return res.render('pages/product-details', { productDetails })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
